432fe8742bce57b06958b4f398e90096c2a0cac4
sbor0018 <sbor0018@student.monash.edu>
2022-08-06 02:08:27 +0000

    Update task1.asm

--- task1.asm
+++ task1.asm
@@ -14,6 +14,12 @@ final_bill_message: .asciiz "Mr Loki Laufeyson, your electricity bill is $"
 full_stop:          .asciiz "."
 new_line:           .asciiz "\n"
 
+# Variables
+age:                .word 0
+consumption:        .word 0
+total_cost:         .word 0
+gst:                .word 0
+total_bill:         .word 0
     .text
 # Print the Welcome Message.
 addi, $v0, $0, 4
@@ -30,24 +36,26 @@ syscall
 addi, $v0, $0, 5
 syscall
 
-# Save age in $t0.
-la $t0, ($v0) # $t0 stores age.
+# Save age
+sw $v0, age
 
 # First if-else statement.
 if1:
-addi $s0, $0, 18
-slt $s1, $s0, $t0
-bne $s1, $0, OR
+lw $t0, age
+addi $t1, $0, 18
+slt $t1, $t1, $t0
+bne $t1, $0, OR
 j then1
 
 OR:
-addi $s0, $0, 65
-slt $s1, $t0, $s0
-bne $s1, $0, else1
+lw $t0, age
+addi $t1, $0, 65
+slt $t1, $t0, $t1
+bne $t1, $0, else1
 
 then1:
-addi $s0, $0, 1
-sw $s0, discount_flag
+addi $t0, $0, 1
+sw $t0, discount_flag
 j consumptionSection
 
 else1:
@@ -63,118 +71,153 @@ syscall
 addi, $v0, $0, 5
 syscall
 
-# Save consumption in $t1.
-la $t1, ($v0) # $t1 stores consumption.
-
-addi $t2, $0, 0 # $t2 stores total cost.
+# Save consumption
+sw $v0, consumption
+sw $0, total_cost # total_cost = 0
 
 # Second if-elif statement.
 if2:
-addi $s0, $0, 1000
-slt $s1, $s0, $t1 # Check if 1000 < consumption.
-bne $s1, $0, AND # Branch to AND if true.
+addi $t0, $0, 1000
+lw $t1, consumption
+slt $t0, $t0, $t1 # Check if 1000 < consumption.
+bne $t0, $0, AND # Branch to AND if true.
 j elif2
 
 AND:
-lw $s0, discount_flag
-beq $s0, $0, if2_then
+lw $t0, discount_flag
+beq $t0, $0, if2_then
 j elif2
 
 if2_then:
-addi $s0, $0, 1000 
-sub $s1, $t1, $s0 # consumption - 1000
-lw $s2, tier_three_price # $s2 = tier_three_price
-mult $s1, $s2 # (consumption - 1000) * tier_three_price
-mflo $s3
-add $t2, $t2, $s3 # total_cost = total_cost + ((consumption-1000)*tier_three_price)
-ori $t1, $0, 1000# set consumption stored in $t1 to 1000
+addi $t0, $0, 1000 
+lw $t1, consumption
+sub $t1, $t1, $t0 # consumption - 1000
+lw $t0, tier_three_price # $t0 = tier_three_price
+mult $t1, $t0 # (consumption - 1000) * tier_three_price
+mflo $t1
+lw $t0, total_cost
+add $t0, $t0, $t1 # total_cost = total_cost + ((consumption-1000)*tier_three_price)
+sw $t0, total_cost
+addi $t0, $0, 1000# set consumption 1000
+sw $t0, consumption
 j if3
 
 elif2:
-addi $s0, $0, 1000
-slt $s1, $s0, $t1 # Check if 1000 < consumption.
-bne $s1, $0, elif2_then # Branch to elif2_then if true.
+addi $t0, $0, 1000
+lw $t1, consumption
+slt $t0, $t0, $t1 # Check if 1000 < consumption.
+bne $t0, $0, elif2_then # Branch to elif2_then if true.
 j if3
 
 elif2_then:
-addi $s0, $0, 1000 
-sub $s1, $t1, $s0 # consumption - 1000
-lw $s2, tier_three_price # $s2 = tier_three_price
-ori $s3, $0, 2
-sub $s2, $s2, $s3 # $s2 = tier_three_price - 2
-mult $s1, $s2 # (consumption - 1000) * (tier_three_price-2)
-mflo $s4 # $s4 = (consumption - 1000) * (tier_three_price-2)
-add $t2, $t2, $s4 # total_cost = total_cost + ((consumption-1000)*(tier_three_price-2))
-ori $t1, $0, 1000 # set consumption stored in $t1 to 1000
+addi $t0, $0, 1000 
+lw $t1, consumption
+sub $t0, $t1, $t0 # consumption - 1000
+lw $t1, tier_three_price # $t1 = tier_three_price
+addi $t1, $0, -2 # $t1 = tier_three_price - 2
+mult $t0, $t1 # (consumption - 1000) * (tier_three_price-2)
+mflo $t0 # $t0 = (consumption - 1000) * (tier_three_price-2)
+lw $t1, total_cost
+add $t1, $t1, $t0 # total_cost = total_cost + ((consumption-1000)*(tier_three_price-2))
+sw $t1, total_cost # save total_cost
+addi $t1, $0, 1000 # set consumption to 1000
+sw $t1, consumption
 
 if3:
 # Third if-else statement.
-addi $s0, $0, 600
-slt $s1, $s0, $t1 # Check if 600 < consumption.
-bne $s1, $0, if3_then # Branch to then if true.
+addi $t0, $0, 600
+lw $t1, consumption
+slt $t0, $t0, $t1 # Check if 600 < consumption.
+bne $t0, $0, if3_then # Branch to then if true.
 j else3
 
 if3_then:
-addi $s0, $0, 600 
-sub $s1, $t1, $s0 # consumption - 600
-lw $s2, tier_two_price # $s2 = tier_two_price
-mult $s1, $s2 # (consumption - 600) * tier_two_price
-mflo $s3
-add $t2, $t2, $s3 # total_cost = total_cost + ((consumption-600)*tier_two_price)
-ori $t1, $0, 600# set consumption stored in $t1 to 600
+addi $t0, $0, 600 
+lw $t1, consumption
+sub $t0, $t1, $t0 # consumption - 600
+lw $t1, tier_two_price # $t1= tier_two_price
+mult $t0, $t1 # (consumption - 600) * tier_two_price
+mflo $t0
+lw $t1, total_cost
+add $t1, $t1, $t0 # total_cost = total_cost + ((consumption-600)*tier_two_price)
+sw $t1, total_cost
+addi $t1, $0, 600#  set consumption to 600
+sw $t1, consumption
 j final_calculations
 
-
 else3:
-addi $s0, $0, 600 
-sub $s1, $t1, $s0 # consumption - 600
-lw $s2, tier_two_price # $s2 = tier_two_price
-ori $s3, $0, 2
-sub $s2, $s2, $s3 # $s2 = tier_three_price - 2
-mult $s1, $s2 # (consumption - 1000) * (tier_two_price-2)
-mflo $s4 # $s4 = (consumption - 1000) * (tier_two_price-2)
-add $t2, $t2, $s4 # total_cost = total_cost + ((consumption-600)*(tier_two_price-2))
-ori $t1, $0, 600 # set consumption stored in $t1 to 600
+addi $t0, $0, 600 
+lw $t1, consumption
+sub $t0, $t1, $t0 # consumption - 600
+lw $t1, tier_two_price # $t1 = tier_two_price
+addi $t1, $t1, -2 # $t1 = tier_three_price - 2
+mult $t0, $t1 # (consumption - 600) * (tier_two_price-2)
+mflo $t0 # $t0 = (consumption - 600) * (tier_two_price-2)
+lw $t1, total_cost
+add $t1, $t1, $t0 # total_cost = total_cost + ((consumption-600)*(tier_two_price-2))
+sw $t1, total_cost
+addi $t1, $0, 600 #  set consumption to 600
+sw $t1, consumption
 
 final_calculations:
-lw $s2, tier_one_price # $s2 = tier_one_price
-mult $t1, $s2 # (consumption * tier_one_price)
-mflo $s4 # $s4 = consumption*tier_one_price
-add $t2, $t2, $s4 # total_cost = total_cost + (consumption*tier_one_price)
+lw $t0, tier_one_price # t0 = tier_one_price
+lw $t1, consumption
+mult $t1, $t0 # (consumption * tier_one_price)
+mflo $t0 # $t0 = consumption*tier_one_price
+lw $t1, total_cost
+add $t1, $t1, $t0 # total_cost = total_cost + (consumption*tier_one_price)
+sw $t1, total_cost
 
-addi $s0, $0, 10
-div $t2, $s0 # total_cost // 10.
-mflo $t3 # gst stored in $t3.
+addi $t0, $0, 10
+lw $t1, total_cost
+div $t1, $t0 # total_cost // 10.
+mflo $t1 # store in gst label
+sw $t1, gst
 
 # total_bill stored in $t4
-add $t4, $t2, $t3 # total_bill = total_cost + gst.
-
-addi $s0, $0, 100
-div $t4, $s0 # total_bill // 100.
-mflo $s1 # total_bill // 100 stored in $s1.
-mfhi $s2 # total_bill % 100 stored in $s2.
+lw $t0, total_cost
+lw $t1, gst
+add $t1, $t0, $t1 # total_bill = total_cost + gst.
+sw $t1, total_bill
 
 final_message_printing:
 addi, $v0, $0, 4
 la $a0, final_bill_message # print final_bill_message string.
 syscall
 
+addi $t0, $0, 100
+lw $t1, total_bill
+div $t1, $t0 # total_bill // 100.
+mflo $t0 # total_bill // 100 stored in t0.
+
 addi $v0, $0, 1
-add $a0, $0, $s1 # print total_bill // 100 
+add $a0, $0, $t0 # print total_bill // 100 
 syscall
 
 addi, $v0, $0, 4
 la $a0, full_stop # print full_stop.
 syscall
 
+addi $t0, $0, 100
+lw $t1, total_bill
+div $t1, $t0 # total_bill // 100.
+mfhi $t0 # total_bill % 100 stored in $t0
+
 addi $v0, $0, 1
-add $a0, $0, $s2 # print total_bill % 100 
+add $a0, $0, $t0 # print total_bill % 100 
 syscall
 
 addi, $v0, $0, 4
-la $a0, new_line # print new line character.
+la $a0, new_line # print new line.
 syscall
 
 End:
 addi, $v0, $0, 10 # End the program
 syscall
+
+
+
+
+
+
+
