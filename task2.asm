4e1c49c5f4594d4c713a6daad989a0ebb2de50ed
sbor0018 <sbor0018@student.monash.edu>
2022-08-06 02:30:58 +0000

    Update task2.asm

--- task2.asm
+++ task2.asm
@@ -16,9 +16,8 @@ next_line:      .asciiz "\n"
 while:
 
 # valid_input stored in $t1.
-lw $t1, valid_input # valid_input loaded into $t1.
-
-bne $t1, $0, outer_for # the counter i stored in $s0. # check if valid_input is 0. Continue loop if true.
+lw $t0, valid_input # valid_input loaded into $t0.
+bne $t0, $0, outer_for # check if valid_input is 0. Continue loop if true.
 
 addi, $v0, $0, 4
 la $a0, height_prompt # Print height prompt message.
@@ -26,47 +25,45 @@ syscall
 
 addi, $v0, $0, 5 # Get the height.
 syscall
-
 sw $v0, height # Store the height value in variable.
 
 # First if-then statement.
 if1:
 lw $t0, height # Height loaded into $t0.
-addi $s0, $0, 5
-slt $s1, $t0, $s0 # Check if height >= 5.
-bne $s1, $0, while
+addi $t1, $0, 5
+slt $t0, $t0, $t1 # Check if height >= 5.
+bne $t0, $0, while
 
 then1:
-addi, $t1, $0, 1 # Set invalid_input to 1.
-sw $t1, valid_input
+addi, $t0, $0, 1 # Set invalid_input to 1.
+sw $t0, valid_input
 j while
 
 outer_for:
-lw $s0, i # the counter i loaded in $s0.
-lw $t0, height # Height loaded into $t0.
-beq $s0, $t0, End # End outer for loop if i is equal to height. End the program if true.
+lw $t0, i # the counter i loaded in $t0.
+lw $t1, height # Height loaded into $t1.
+beq $t0, $t1, End # End outer for loop if i is equal to height. End the program if true.
 
 # Initialise s counter.
 lw $t0, height # Height loaded into $t0.
-addi $s0, $t0, 1 # the counter s stored in $s1. (height+1)
-addi $s1, $0, -1 # Load -1 into $s1
-mult $s1, $s0 # Doing this operation: (height+1)*-1
-mflo $s2 # $s2 = (height+1)*-1
-sw $s2, s # save (height+1)*-1 in s.
+addi $t0, $t0, 1 # (height+1)
+addi $t1, $0, -1 # Load -1 into $t1
+mult $t0, $t1 # Doing this operation: (height+1)*-1
+mflo $t0 # $t0 = (height+1)*-1
+sw $t0, s # save (height+1)*-1 in s.
 
 # Initialise j counter to 0.
-addi $s0, $0, 0
-sw $s0, j
+addi $t0, $0, 0
+sw $t0, j
 
 inner_for1:
+addi $t0, $0, -1
+lw $t1, i # load current i value into $s0.
+mult $t0, $t1
+mflo $t0 # $s2 = -i
 
-addi $s0, $0, -1
-lw $s1, i # load current i value into $s0.
-mult $s0, $s1
-mflo $s2 # $s2 = -i
-
-lw $s3, s # load current s value into $s3.
-beq $s3, $s2, inner_for2
+lw $t1, s # load current s value into $s3.
+beq $t1, $t0, inner_for2
 
 # Print SPACE
 addi, $v0, $0, 4
@@ -74,22 +71,22 @@ la $a0, space # Print SPACE.
 syscall
 
 # Increment counter s. 
-lw $s0, s # load current s value into $s0.
-addi $s0, $s0, 1 #(s = s + 1)
-sw $s0, s # save new s counter value.
+lw $t0, s # load current s value into $t0.
+addi $t0, $t0, 1 #(s = s + 1)
+sw $t0, s # save new s counter value.
 j inner_for1
 
 inner_for2:
-lw $s0, j # The counter j stored in $s0.
-lw $s1, i # load current i value into $s1.
-addi $s2, $s1, 1
+lw $t0, j # The counter j stored in $t0.
+lw $t1, i # load current i value into $t1.
+addi $t1, $t1, 1 # i + 1
 
-beq $s0, $s2, end_inner_for2
+beq $t0, $t1, end_inner_for2 # j = i + 1?
 
 # IF-ELSE-PRINT
 if2:
-lw $s0, i # load current i value into $s1.
-bne $s0, $0, else2
+lw $t0, i # load current i value into $s1.
+bne $t0, $0, else2
 if2_then:
 addi, $v0, $0, 4
 la $a0, alphabet_A # Print A.
@@ -103,9 +100,9 @@ la $a0, asterisk # Print *.
 syscall
 
 end_if2:
-lw $s0, j
-addi $s0, $s0, 1 # Increment counter j. (j = j + 1)
-sw $s0, j
+lw $t0, j
+addi $t0, $t0, 1 # Increment counter j. (j = j + 1)
+sw $t0, j
 j inner_for2
 
 end_inner_for2:
@@ -114,9 +111,9 @@ addi, $v0, $0, 4
 la $a0, next_line # Print \n .
 syscall
 
-lw $s0, i # load i value.
-addi $s0, $s0, 1 # Increment counter i. (i = i +  1)
-sw $s0, i # save incremented i value.
+lw $t0, i # load i value.
+addi $t0, $t0, 1 # Increment counter i. (i = i +  1)
+sw $t0, i # save incremented i value.
 j outer_for
 
 End:
