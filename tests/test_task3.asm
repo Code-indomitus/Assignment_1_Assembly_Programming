5e83d45c30bc7a3f32a587d9978742b03928f9cc
sbor0018 <sbor0018@student.monash.edu>
2022-08-13 02:51:21 +0000

    Function smash_or_sad added

--- tests/test_task3.asm
+++ tests/test_task3.asm
@@ -5,6 +5,9 @@ prompt_num: .asciiz "Enter num: "
 hp_prompt:	.asciiz "Enter the value of hulk power: "
 main_str:	.asciiz "Hulk smashed "
 sub_str:	.asciiz " people"
+hulk_smash:     .asciiz "Hulk SMASH! >:(\n"
+hulk_sad:       .asciiz "Hulk Sad :(\n" 
+
 size:       .word   0
 i:			.word 	0
 hp:			.word   0
@@ -111,3 +114,69 @@ endfor:		addi $sp, $sp, -8 # make space for 2 arguments
 			# Exit the program
 			addi $v0, $0, 10  # $v0 = 10 for exiting the program
 			syscall
+
+smash_or_sad:
+            # space for $ra and $fp on stack.
+			addi $sp, $sp, -8
+			# save $ra onto stack.
+			sw $ra, 4($sp)
+			# save $fp onto stack.
+			sw $fp, 0($sp)
+			# copy $sp into $fp.
+			addi $fp, $sp, 0
+
+			# allocate space for two local variables and push that local variables. (smash_count and i counter)
+			addi $sp, $sp, -8
+			addi $t0, $0, 0 # smash_count = 0
+			sw $t0, -4($fp)
+			addi $t0, $0, 0 # i = 0
+			sw $t0, -8($fp)
+
+for: 
+			lw $t0, -8($fp)# load i from stack
+			lw $t1, 12($fp) # load address of the_list
+			lw $t1, 0($t1)# load len of the_list (first element of array)
+			slt $t0, $t0, $t1 # i < len(the_list)?
+			beq $t0, $0, end_for # end for loop if not true
+
+if:
+			lw $t0, -8($fp)# load i from stack
+			addi $t0, $t0, 1 # add 1 to consider first element being size.
+			sll $t0, $t0, 2 # multiply by 4
+			lw $t1, 12($fp) # load address of the_list
+			add $t1, $t0, $t1 # address of the_list[i]
+			lw $t0, ($t1) # the_list[i]
+			lw $t1, 8($fp) # load hulk_power
+			slt $t0, $t1, $t0 # not(hulk_power < the_list[i])
+			bne $t0, $0, else
+
+			then:
+			addi $v0, $0, 4
+			la $a0, hulk_smash
+			syscall
+			lw $t0, -4($fp) # load smash_count
+			addi $t0, $t0, 1 # increment smash_count
+			sw $t0, -4($fp) # update smash_count in the stack
+			j end_if
+
+else:
+			addi $v0, $0, 4
+			la $a0, hulk_sad
+			syscall
+
+end_if:
+			lw $t0, -8($fp)# load i from stack
+			addi $t0, $t0, 1 # increment i counter
+			sw $t0, -8($fp) # update i counter in the stack
+			j for
+
+end_for:
+			# Return smash_count. Set $v0 to return value.
+			lw $v0, -4($fp)
+			addi $sp, $sp, 8 # clear local variables off the stack
+			lw $fp, 0($sp) # restore $fp
+			lw $ra, 4($sp) # restore $ra
+			addi $sp, $sp, 8 # clear $fp and $ra off the stack
+			jr $ra
+
+
