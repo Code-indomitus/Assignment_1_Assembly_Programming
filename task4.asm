bf8fefcec854a8b8298226c10cdc0252543e0686
sbor0018 <sbor0018@student.monash.edu>
2022-08-14 12:46:57 +0000

    Order of functions changed

--- task4.asm
+++ task4.asm
@@ -11,95 +11,6 @@ new_line:    .asciiz "\n"
             addi $v0, $0, 10
             syscall
 
-main:
-            # space for $ra and $fp on stack.
-            addi $sp, $sp, -8
-            # save $ra onto stack.
-            sw $ra, 4($sp)
-            # save $fp onto stack.
-            sw $fp, 0($sp)
-            # copy $sp into $fp.
-            addi $fp, $sp, 0
-            # allocate space for one local variables.
-            addi $sp, $sp, -4
-
-            # allocate space for arr.
-            addi $v0, $0, 9
-            addi $a0, $0, 24 # (5 + 1) * 4 bytes must be reserved.
-            syscall
-            addi $t0, $v0, 0 # copy address of arr to $t0
-            # push address of array to stack
-            sw $t0, -4($fp)
-
-            lw $t0, -4($fp) # load address of array
-            addi $t1, $0, 5 # Assign start of array to size of array.
-            sw $t1, ($t0) # total length of array = 5 + 1
-            # Fill in the elements into the array
-            addi $t1, $0, 6
-            sw $t1, 4($t0) # arr[0] = 6
-            addi $t1, $0, -2
-            sw $t1, 8($t0) # arr[1] = -2
-            addi $t1, $0, 7
-            sw $t1, 12($t0) # arr[2] = 7
-            addi $t1, $0, 4
-            sw $t1, 16($t0) # arr[3] = 4
-            addi $t1, $0, -10
-            sw $t1, 20($t0) # arr[4] = -10
-
-            # allocate space for 1 argument
-            addi $sp, $sp, -4
-            # pushing arguments onto stack
-            lw $t0, -4($fp) # push arr argument as the_list
-            sw $t0, 0($sp)
-
-            # call insertion_sort
-            jal insertion_sort 
-            addi $sp, $sp, 4 # clear argument the_list off the stack
-
-            addi $sp, $sp, -4 # push new variable i onto stack
-            addi $t0, $0, 0 # i = 0
-            sw $t0, -8($fp)
-
-for_main:
-            lw $t0, -8($fp) # load i from stack
-            lw $t1, -4($fp) # get address of arr
-            lw $t1, 0($t1) # get size/length of array arr
-            slt $t0, $t0, $t1 # i < len(arr)?
-            beq $t0, $0, end_for_main
-
-            lw $t0, -8($fp)# load i from stack
-            addi $t0, $t0, 1 # add 1 to consider first element being size.
-            sll $t0, $t0, 2 # multiply by 4
-            lw $t1, -4($fp) # load address of arr
-            add $t1, $t0, $t1 # address of arr[i]
-            lw $t0, ($t1) # arr[i] in $t0
-
-            # print (arr[i], end=" ")
-            addi $v0, $0, 1
-            addi $a0, $t0, 0# print arr[i]
-            syscall
-
-            addi $v0, $0, 4
-            la $a0, space # print space
-            syscall
-
-            lw $t0, -8($fp) # load i from stack
-            addi $t0, $t0, 1 # increment i
-            sw $t0, -8($fp) # save new i
-            j for_main
-
-end_for_main:
-            addi $v0, $0, 4
-            la $a0, new_line # print new_line
-            syscall
-
-            addi $sp, $sp, 8 # clear local variable arr and i off the stack
-            lw $fp, 0($sp) # restore $fp
-            lw $ra, 4($sp) # restore $ra
-            addi $sp, $sp, 8 # restore stack pointer to original
-
-            jr $ra
-
 insertion_sort:
             # space for $ra and $fp on stack.
             addi $sp, $sp, -8
@@ -200,3 +111,91 @@ end_for_sort:
             addi $sp, $sp, 8 # clear $fp and $ra off the stack
             jr $ra
 
+main:
+            # space for $ra and $fp on stack.
+            addi $sp, $sp, -8
+            # save $ra onto stack.
+            sw $ra, 4($sp)
+            # save $fp onto stack.
+            sw $fp, 0($sp)
+            # copy $sp into $fp.
+            addi $fp, $sp, 0
+            # allocate space for one local variables.
+            addi $sp, $sp, -4
+
+            # allocate space for arr.
+            addi $v0, $0, 9
+            addi $a0, $0, 24 # (5 + 1) * 4 bytes must be reserved.
+            syscall
+            addi $t0, $v0, 0 # copy address of arr to $t0
+            # push address of array to stack
+            sw $t0, -4($fp)
+
+            lw $t0, -4($fp) # load address of array
+            addi $t1, $0, 5 # Assign start of array to size of array.
+            sw $t1, ($t0) # total length of array = 5 + 1
+            # Fill in the elements into the array
+            addi $t1, $0, 6
+            sw $t1, 4($t0) # arr[0] = 6
+            addi $t1, $0, -2
+            sw $t1, 8($t0) # arr[1] = -2
+            addi $t1, $0, 7
+            sw $t1, 12($t0) # arr[2] = 7
+            addi $t1, $0, 4
+            sw $t1, 16($t0) # arr[3] = 4
+            addi $t1, $0, -10
+            sw $t1, 20($t0) # arr[4] = -10
+
+            # allocate space for 1 argument
+            addi $sp, $sp, -4
+            # pushing arguments onto stack
+            lw $t0, -4($fp) # push arr argument as the_list
+            sw $t0, 0($sp)
+
+            # call insertion_sort
+            jal insertion_sort 
+            addi $sp, $sp, 4 # clear argument the_list off the stack
+
+            addi $sp, $sp, -4 # push new variable i onto stack
+            addi $t0, $0, 0 # i = 0
+            sw $t0, -8($fp)
+
+for_main:
+            lw $t0, -8($fp) # load i from stack
+            lw $t1, -4($fp) # get address of arr
+            lw $t1, 0($t1) # get size/length of array arr
+            slt $t0, $t0, $t1 # i < len(arr)?
+            beq $t0, $0, end_for_main
+
+            lw $t0, -8($fp)# load i from stack
+            addi $t0, $t0, 1 # add 1 to consider first element being size.
+            sll $t0, $t0, 2 # multiply by 4
+            lw $t1, -4($fp) # load address of arr
+            add $t1, $t0, $t1 # address of arr[i]
+            lw $t0, ($t1) # arr[i] in $t0
+
+            # print (arr[i], end=" ")
+            addi $v0, $0, 1
+            addi $a0, $t0, 0# print arr[i]
+            syscall
+
+            addi $v0, $0, 4
+            la $a0, space # print space
+            syscall
+
+            lw $t0, -8($fp) # load i from stack
+            addi $t0, $t0, 1 # increment i
+            sw $t0, -8($fp) # save new i
+            j for_main
+
+end_for_main:
+            addi $v0, $0, 4
+            la $a0, new_line # print new_line
+            syscall
+
+            addi $sp, $sp, 8 # clear local variable arr and i off the stack
+            lw $fp, 0($sp) # restore $fp
+            lw $ra, 4($sp) # restore $ra
+            addi $sp, $sp, 8 # restore stack pointer to original
+
+            jr $ra
